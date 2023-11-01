62af88b01e045fc71b6a46b1d48b053d4968dca8
Oguzhan Sofuoglu <oguzhan.sofuoglu@ciceksepeti.com>
2023-11-01 15:13:10 +0300

    feat: add description for loading ui

--- src/app/loading-ui/loading.tsx
+++ src/app/loading-ui/loading.tsx
@@ -1,7 +1,13 @@
 export default function Loading() {
   return (
-    <div className="w-full text-center flex align-center justify-center mt-32">
-      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    <div>
+      <div className="w-full text-center flex align-center justify-center mt-32">
+        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+      </div>
+      <p className="w-full text-center text-xl flex align-center justify-center mt-32">
+        This indicator means that server side rendering is still processing. Requested HTML will be
+        displayed when rendering completed.
+      </p>
     </div>
   );
 }
