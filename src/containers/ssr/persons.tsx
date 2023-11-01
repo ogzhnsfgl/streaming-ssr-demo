3023e82ac9e234a265e1f36610b7b051a8617ed7
Oguzhan Sofuoglu <oguzhan.sofuoglu@ciceksepeti.com>
2023-11-01 21:18:11 +0300

    fix: status delays reduced

--- src/containers/ssr/persons.tsx
+++ src/containers/ssr/persons.tsx
@@ -4,12 +4,12 @@ import { DevCard } from '@/components/dev-card';
 export default async function Persons() {
   return (
     <CardList title="Persons">
-      <DevCard delay={200} />
-      <DevCard delay={300} />
+      <DevCard delay={1100} />
+      <DevCard delay={4500} />
       <DevCard delay={500} />
-      <DevCard delay={600} />
-      <DevCard delay={200} />
-      <DevCard delay={100} />
+      <DevCard delay={2200} />
+      <DevCard delay={300} />
+      <DevCard delay={3000} />
     </CardList>
   );
 }
